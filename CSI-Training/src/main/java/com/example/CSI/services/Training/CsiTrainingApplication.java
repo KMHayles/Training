5e131ff5985d02f796f63e22146cb7d6fbaf0647
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-05-09 23:22:11 -0500

    setting up rep

---
+++ CSI-Training/src/main/java/com/example/CSI/services/Training/CsiTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.CSI.Training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsiTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsiTrainingApplication.class, args);
+	}
+
+}
---
+++ CSI-Training/src/test/java/com/example/CSI/Training/CsiTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CSI.Training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsiTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
